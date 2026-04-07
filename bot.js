6dcbae826feadeca7bfdaa1d4485303c938e43fe
ikhsanf71 <if280297@gmail.com>
2026-04-07 13:39:28 +0700

    first commit

---
+++ bot.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+const TelegramBot = require('node-telegram-bot-api');
+
+const token = process.env.BOT_TOKEN;
+const bot = new TelegramBot(token, { polling: true });
+
+bot.on('message', (msg) => {
+  bot.sendMessage(msg.chat.id, 'Bot aktif bro 🔥');
+});
